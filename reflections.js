ac4bb6fae778d13b936f753c475b0c237733dfb5
Takeallah <takeallahsrivera@icloud.com>
2020-03-06 14:25:26 -0600

    PushFour

--- reflections.js
+++ reflections.js
@@ -1,4 +1,11 @@
-alert ("Please submit your daily reflections, Takeallah!");
+var submission= prompt('Hi! Do you have a reflection to submit, Takeallah?');
+if (submission == 'yes'){
+    alert('Awesome! We look forward to reading your reflection,     Takeallah!'); {
+ if (submission != 'yes'){
+    alert('Bummer! Maybe next time, Takeallah!');
+}
+    }
+}
 
 /*let journalentries = {
 
@@ -9,4 +16,3 @@ alert ("Please submit your daily reflections, Takeallah!");
 
 }
 */
- document.getElementById("MyButton").onclick='Do Something';
