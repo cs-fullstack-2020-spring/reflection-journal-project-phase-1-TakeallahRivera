b31459dbcab663fd7e306b0cf7b1e6be1a255dcb
Takeallah <takeallahsrivera@icloud.com>
2020-03-06 15:33:09 -0600

    PushFive

--- reflections.js
+++ reflections.js
@@ -1,18 +1,9 @@
 var submission= prompt('Hi! Do you have a reflection to submit, Takeallah?');
 if (submission == 'yes'){
     alert('Awesome! We look forward to reading your reflection,     Takeallah!'); {
- if (submission != 'yes'){
+ if (submission !== 'yes'){
     alert('Bummer! Maybe next time, Takeallah!');
 }
     }
 }
 
-/*let journalentries = {
-
-    devsname
-    createdate
-    journalentry
-    confidlevel
-
-}
-*/
