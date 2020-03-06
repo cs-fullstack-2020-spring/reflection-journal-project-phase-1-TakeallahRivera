363405661ce36faf2996d5c87b6e1b46e44f9e1f
Takeallah <takeallahsrivera@icloud.com>
2020-03-06 15:58:43 -0600

    PushFive

--- reflections.js
+++ reflections.js
@@ -1,3 +1,5 @@
+//Inserted prompts for extra practice with JavaScript and for a little extra razzle dazzle
+
 var submission= prompt('Hi! Do you have a reflection to submit, Takeallah?');
 if (submission == 'yes'){
     alert('Awesome! We look forward to reading your reflection,     Takeallah!'); {
